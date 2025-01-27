56d766674fb82e71fafbb09255400f245cc5248a
Gabe-Ptz <gabe.portz.3007@gmail.com>
2025-01-27 00:54:55 -0300

    jn

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const swicher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('dark-theme')
+    var className = document.body.className;
+    if(className = "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light"
+    }
+    console.log('current class name:' + className);
 });
